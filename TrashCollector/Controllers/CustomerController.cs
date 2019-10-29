b6de8a84c39602c97eeb6c5afa98dda284349c6f
Jake Navarro <jt10navarro@gmail.com>
2019-10-29 17:51:38 -0500

    added redirect to create customer and employee afte registering.  confirmed they show up in asp.net and customer/employee db's

--- TrashCollector/Controllers/CustomerController.cs
+++ TrashCollector/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -41,6 +42,8 @@ namespace TrashCollector.Controllers
             try
             {
                 // TODO: Add insert logic here
+                string id = User.Identity.GetUserId();
+                customer.ApplicationId = id;
                 context.Customers.Add(customer);
                 context.SaveChanges();
 
--- TrashCollector/Startup.cs
+++ TrashCollector/Startup.cs
@@ -12,6 +12,7 @@ namespace TrashCollector
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesandUsers();
         }
 
         private void createRolesandUsers()
@@ -21,23 +22,50 @@ namespace TrashCollector
             var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
             var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
 
+            // In Startup iam creating first Admin Role and creating a default Admin User    
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin rool   
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                  
+
+                var user = new ApplicationUser();
+                user.UserName = "Jake";
+                user.Email = "jt10navarro@gmail.com";
+
+                string userPWD = "devcode";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin   
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                }
+            }
+
             // creating Creating Manager role    
-            if (!roleManager.RoleExists("Manager"))
-            {
-                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
-                role.Name = "Manager";
-                roleManager.Create(role);
+            if (!roleManager.RoleExists("Customer"))
+                {
+                    var role = new IdentityRole();
+                    role.Name = "Customer";
+                    roleManager.Create(role);
 
+                }
+
+                // creating Creating Employee role    
+                if (!roleManager.RoleExists("Employee"))
+                {
+                    var role = new IdentityRole();
+                    role.Name = "Employee";
+                    roleManager.Create(role);
+
+                }
             }
-
-            // creating Creating Employee role    
-            if (!roleManager.RoleExists("Employee"))
-            {
-                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
-                role.Name = "Employee";
-                roleManager.Create(role);
-
-            }
-        }
     }
 }
